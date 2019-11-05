2bde7c3d1618554474fcbc0bab2b403e02b61522
Fenoxe <atte@berkeley.edu>
2019-11-05 07:35:21 -0800

    created side to side test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from DriverInterface import DriverInterface
 import time
 from Move import Move
+import Config as config
 
 def di_test():
     d = DriverInterface()
@@ -117,6 +118,33 @@ def basic_test(step_delay, step_count, direc, ms):
 
     d.set_step(ms)
     d.set_dir(direc)
+    d.execute(step_delay, step_count)
+
+    d.sleep(1)
+
+    print("Finished.")
+
+def side_to_side(step_delay, ms):
+    print("Side to side test started")
+
+    d = DriverInterface()
+
+    dist = 0.3 # +/-
+    step_count = dist * config.DIST_TO_STEPS
+    d.set_step(ms)
+    
+    #prologue
+    d.set_dir(0)
+    d.execute(step_delay, step_count)
+    for _ in range(5):
+        d.set_dir(0)
+        d.execute(step_delay, 2 * step_count)
+
+        time.sleep(0.3)
+
+        d.set_dir(1)
+        d.execute(step_delay, 2 * step_count)
+    d.set_dir(0)
     d.execute(step_delay, step_count)
 
     d.sleep(1)
