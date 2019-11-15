75a9c822630e8a04873a510603ba2dc3c342f24a
Fenoxe <atte@berkeley.edu>
2019-11-15 08:33:32 -0800

    flip input

--- DriverInterface.py
+++ DriverInterface.py
@@ -55,7 +55,7 @@ class DriverInterface:
             time.sleep(delay)
             GPIO.output(PINS.STEP, GPIO.LOW)
             time.sleep(delay)
-            if GPIO.input(PINS.INITSTOP):
+            if not GPIO.input(PINS.INITSTOP):
                 break
         time.sleep(0.5)
         self.enable(0)
